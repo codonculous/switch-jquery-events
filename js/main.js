d8808871049e9a8e9090dd903cf7ee62865bfb0c
codonculous <feidameng01@gmail.com>
2017-04-29 19:18:10 -0400

    change the text of h1.status when button is clicked

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ $(function() {
     var switchStatus = $('.plate button').attr('class');
     if (switchStatus === 'switch on') {
       $('.plate button').attr('class','switch off');
-       $('body').attr('class','dark');
+      $('body').attr('class','dark');
+      $('h1.status').text('Hey, who turned off the lights?');
     } else {
       $('.plate button').attr('class','switch on');
       $('body').attr('class','light');
+      $('h1.status').text("It's so bright in here!");
     }
 
 
