b4c32887bc187707785faf4d0b1df0e4f0e39259
codonculous <feidameng01@gmail.com>
2017-04-29 19:15:39 -0400

    create if conditions to swtich the class of the button when its clicked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+$(function() {
+
+  $('.plate button').on('click',function() {
+    var switchStatus = $('.plate button').attr('class');
+    if (switchStatus === 'switch on') {
+      $('.plate button').attr('class','switch off');
+    } else {
+      $('.plate button').attr('class','switch on');
+
+    }
 
 
+  });
+
+});
