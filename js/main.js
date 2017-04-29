300d403d05e5e4030974368ab81d6d42ab3abb02
codonculous <feidameng01@gmail.com>
2017-04-29 19:16:46 -0400

    change body class when the button is clicked

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ $(function() {
     var switchStatus = $('.plate button').attr('class');
     if (switchStatus === 'switch on') {
       $('.plate button').attr('class','switch off');
+       $('body').attr('class','dark');
     } else {
       $('.plate button').attr('class','switch on');
-
+      $('body').attr('class','light');
     }
 
 
